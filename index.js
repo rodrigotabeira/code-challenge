6b8ae26d9ac3a359fcd5395c129b2366394ae1e8
rodrigotabeira <tabeirarodrigo8@gmail.com>
2023-09-05 21:12:16 -0300

    comentarios en js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 document.getElementById("FormRegistro").addEventListener("submit", function (f) {
     f.preventDefault();
-
+    
+    // Tomar datos del formulario
     const dataForm = {
         nombre: document.getElementById("name").value,
         apellido: document.getElementById("lastname").value,
         fecha: document.getElementById("birthdate").value,
     };
 
+    // Envia una solicitud POST al servidor
     fetch("https://jsonplaceholder.typicode.com/users.", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(dataForm)
+        body: JSON.stringify(dataForm) // Convierte los datos en formato JSON
     })
-    .then(response => response.json())
+    .then(response => response.json()) // Parsea la respuesta del servidor a JSON
     .then(data => {
-        console.log("Respuesta del servidor:", data);
+        console.log("Respuesta del servidor:", data); // Maneja la respuesta del servidor aquí
     })
     .catch(error => {
-        console.error("Error al mandar los datos", error);
+        console.error("Error al mandar los datos", error); // Maneja errores en caso de problemas en la solicitud
     });
 
 });
